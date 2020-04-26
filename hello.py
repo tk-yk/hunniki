2d9ea16cd07cd297b26c410b5bcad7f317209058
tk-yk <tkmktuuhan@gmail.com>
2020-04-26 19:23:55 +0900

    ゲームのユーザー名を表現するBMIクラスを実装
    ーclassのインスタンス化をした。
    ー"caluculate()"で個人のBMIを管理した。

---
+++ hello.py
@@ -0,0 +1,20 @@
+print('Hello World')
+
+name = ("浅井隆之です。")
+
+print (name)
+
+print(1+2 < 10)
+
+if name != "浅井隆之":
+    print ("正解")
+
+    num = 120
+    num_count = 5
+
+    result_num = num * num_count
+
+    print("合計は"+str(result_num)+"です。")
+
+
+
---
+++ rennsyuu.py
@@ -0,0 +1,41 @@
+import math
+
+print("Hello World")
+
+print("hello".split("o"))
+
+print("Hello World".split("e"))
+
+print("HELLO WORLD".lower())
+
+print("hello world".upper())
+
+print("saodjfo;afej3j3i4j3".upper())
+
+print("Hello world. I am Tack. Nice to meet you, Chop! Let's go swimming. Ha, ha, ha, it's a joke!!".upper().split(" "))
+
+x = "Hello World"
+print(x)
+
+x = "Hello World".split(" ")
+print(x)
+
+y = " my ".join(x)
+
+print(y)
+
+print("asai yuuto".find("u"))
+print("missisippi".count("p"))
+
+print("Hello World".find("el"))
+print("Hello World"[2])
+print("It' a pen.")
+print("It's a pen.")
+print("It's a pen.")
+print("It's a pen.")
+for num in range(1,10):
+    print(num)
+
+    print("hello world\nI am  happy.")
+    
+
